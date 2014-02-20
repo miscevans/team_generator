7f56418d765186cbc5f5f25e75afa9d379fe951b
Nidhi Mehta <nidhididi@gmail.com>
2014-02-20 08:55:18 -0500

    Trying to make a form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ TeamGenerator::Application.routes.draw do
 
   resources :students
 
+  get "/teams/num_teams"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
